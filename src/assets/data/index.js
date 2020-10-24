0575f0d27e84a6812e4a49fd0d93e03651251940
dawidgola <dawidgola7@gmail.com>
2020-10-24 23:52:05 +0200

    #8 - update data

--- src/assets/data/index.js
+++ src/assets/data/index.js
@@ -1,6 +1,5 @@
 import HeadPhonesImage from "../img/headphones.png";
 
 export const data = [
-  { id: 1, name: "HeadPhones", price: 11.9, img: HeadPhonesImage },
-  //   { id: 2, name: "HeadPhones", price: 15.9, img: HeadPhonesImage },
+  { id: 1, name: "HeadPhones", price: 11.9, img: HeadPhonesImage }
 ];
